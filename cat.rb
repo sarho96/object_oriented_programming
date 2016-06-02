047a0d7d017d7f49109f86ed0a96f000ac1418f6
Stella <stellaha4@gmail.com>
2016-06-02 16:14:55 -0400

    added a meow method and output

--- cat.rb
+++ cat.rb
@@ -12,10 +12,13 @@ attr_accessor :name, :preferred_food, :meal_time
   end
   def eats_at
     if meal_time >= 12
-      puts "#{meal_time} PM"
+      "#{meal_time-12} PM"
     else
-      puts "#{meal_time} AM"
+      "#{meal_time} AM"
     end
+  end
+  def meow
+    puts "My name is #{name} and I eat #{preferred_food} at #{eats_at}"
   end
 end
 
@@ -24,4 +27,8 @@ cat1 = Cat.new("Piggy","Fish",2)
 cat2 = Cat.new("Lucy","Dogs",3)
 cat3 = Cat.new("Beeb","Bees",14)
 
+
 puts cat1.eats_at
+puts cat1.meow
+puts cat2.meow
+puts cat3.meow
