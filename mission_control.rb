4dbdf03ba91818602fe191795f1002de9f461775
Stella <stellaha4@gmail.com>
2016-06-05 19:45:25 -0400

    Put more comments in

--- mission_control.rb
+++ mission_control.rb
@@ -1,8 +1,31 @@
-=begin
+require_relative 'rover'
 
-1. create a class called Mission_Control
-2. in this class, initialize it by defining an initialize method
-3. in initialize method, create instance variables which you can put onto attr_accessor
+=begin
+rover1 = Rover.new(0,0,"N")
+
+Method Mission_Control_Command(user_input)
+    1. what is the plateau's size?
+      plateaue_size = gets.chomp
+
+    2. What is the starting position of first rover? (facing_direction, x_coordinate, y_coordinate)
+    starting_position << rover_array
+      - perhaps I can output starting position results through shovelling each instance of rover class as an array
+
+    3. what direction is the rover turning or moving? L or R? M?
+      rover_turning_direction = gets.chomp
+      rover_moving_direction = gets.chomp
+
+puts "rover is starting at (#{@x_coordinate},#{@y_coordinate}) facing #{@facing_direction}."
+puts "Rover will move according to these coordinates: #{rover_turning_direction} and move #{rover_moving_number}"
+if rover is moving to these coordinates, then their final position should be:
+  ... need to find out how to push rover coordinators to add new coordinates in mission_control class
+
+
+
+
+[plataue_size, starting_position[rover_starting_directions], rover_turning_direction, rover_moving_direction]
+      0               1                   2                           3
+
 
 
 =end
--- rover.rb
+++ rover.rb
@@ -3,12 +3,12 @@ class Rover
 
 #2. initialize class rover by creating its instance variables
 #3. put instance variables into attr_accessor
-  attr_accessor :x_coordinate, :y_coordinate, :current_direction
+  attr_accessor :x_coordinate, :y_coordinate, :facing_direction
 
-  def initialize(x_coordinate, y_coordinate, current_direction)
+  def initialize(x_coordinate, y_coordinate, facing_direction)
     @x_coordinate = 0
     @y_coordinate = 0
-    @current_direction = current_direction
+    @facing_direction = facing_direction
   end
 
   #4. def a method called read_instructions
@@ -30,13 +30,13 @@ class Rover
     #use an if statement(turn_direction) to determine if turn_durection == Left or right
     #if Left then move n, e, s, w
     #if Right then move n, e, s, w
-      if (@current_direction == "N" && command == "L") || (@current_direction == "S" && command == "R")
+      if (@facing_direction == "N" && command == "L") || (@facing_direction == "S" && command == "R")
         turn_direction = "W"
-      elsif (@current_direction == "E" && command == "L") || (@current_direction == "W" && command == "R")
+      elsif (@facing_direction == "E" && command == "L") || (@facing_direction == "W" && command == "R")
         turn_direction = "N"
-      elsif (@current_direction == "S" && command == "L") || (@curret_direction == "N" && command == "R")
+      elsif (@facing_direction == "S" && command == "L") || (@curret_direction == "N" && command == "R")
         turn_direction == "E"
-      elsif (@current_direction == "W" && command == "L") || (@current_direction == "E" && command == "R")
+      elsif (@facing_direction == "W" && command == "L") || (@facing_direction == "E" && command == "R")
           turn_direction = "S"
       end
   end
@@ -45,20 +45,22 @@ class Rover
       #create an if statement to determine the direction
       #move method will increment or decrement the x_coordinate or y_coordinate
       #for example: if direction is west, then the position will go down by 1
-    if @current_direction == "N"
+    if @facing_direction == "N"
       @y_coordinate += 1
-    elsif @current_direction == "E"
+    elsif @facing_direction == "E"
       @x_coordinate += 1
-    elsif current_direction == "W"
+    elsif facing_direction == "W"
       @x_coordinate -= 1
-    elsif current_direction == "S"
+    elsif facing_direction == "S"
       @x_coordinate -= 1
     end
   end
 
   def status
-    return "The rover's position is at #{@y_coordinate},#{@x_coordinate}, facing #{@current_direction}"
+    return "The rover's position is at #{@y_coordinate},#{@x_coordinate}, facing #{@facing_direction}"
   end
 end
+
+
 rover1 = Rover.new(0, 1, "W")
 p rover1.status
