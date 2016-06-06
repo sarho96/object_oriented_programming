4a4d6ab37d8512d587fc3f08fe88f476b61e800f
Stella <stellaha4@gmail.com>
2016-06-06 13:22:40 -0400

    re-worded a few things

--- rover.rb
+++ rover.rb
@@ -1,51 +1,42 @@
-#1. create class Rover
 class Rover
 
-#2. initialize class rover by creating its instance variables
-#3. put instance variables into attr_accessor
   attr_accessor :x_coordinate, :y_coordinate, :facing_direction
-  #within instance variables, ensure that x_coordinate and y_coordinate is set to 0
 
   def initialize(x_coordinate, y_coordinate, facing_direction)
-    @x_coordinate = 0
-    @y_coordinate = 0
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
     @facing_direction = facing_direction
+    puts @facing_direction
   end
 
-  #4. def a method called read_instructions
+#4. def a method called read_instructions
   def read_instructions(instructions)
-  #5. read_instructions will direct rover to the move method or turn method
     instructions.each do |command|
       if command == "L" || command == "R"
-        turn
+        turn(command)
       elsif command == "M"
         move
       else
       end
     end
+    return "you are at #{@x_coordinate} #{@y_coordinate} #{@facing_direction}"
   end
 
 #6. def a method called turn
   def turn(turn_direction)
-    #use an if statement(turn_direction) to determine if turn_durection == Left or right
-    #if Left then move n, e, s, w
-    #if Right then move n, e, s, w
       if (@facing_direction == "N" && command == "L") || (@facing_direction == "S" && command == "R")
-        turn_direction = "W"
+        @facing_direction = "W"
       elsif (@facing_direction == "E" && command == "L") || (@facing_direction == "W" && command == "R")
-        turn_direction = "N"
+        @facing_direction = "N"
       elsif (@facing_direction == "S" && command == "L") || (@curret_direction == "N" && command == "R")
-        turn_direction == "E"
+        @facing_direction == "E"
       elsif (@facing_direction == "W" && command == "L") || (@facing_direction == "E" && command == "R")
-          turn_direction = "S"
-      else
+        @facing_direction = "S"
       end
   end
+
 #7. def a method called move
   def move
-      #create an if statement to determine the direction
-      #move method will increment or decrement the x_coordinate or y_coordinate
-      #for example: if direction is west, then the position will go down by 1
     if @facing_direction == "N"
       @y_coordinate += 1
     elsif @facing_direction == "E"
@@ -60,15 +51,21 @@ class Rover
 end
 
 
-puts "What is the plateau size? ex. 5 5"
-plateau_size = gets.chomp.split(" ")
+  puts "What is the plateau size? ex. 5 5"
+  plateau_size = gets.chomp
+  puts "Your plateau size is #{plateau_size}"
 
-puts "What is your initial start position? enter: x-coordinate, y-coordinate, facing direction"
-initial_start_position = gets.chomp.split(" ")
 
-puts "Please enter your move and turn instructions. ex. LMRMMLM"
-move_turn_instructions = gets.chomp.split(" ")
+  puts "What is your initial start position? enter: x-coordinate, y-coordinate, facing direction"
+  initial_start_position = gets.chomp.split(" ")
+  puts "Your starting position is #{initial_start_position}"
 
-rover = Rover.new(initial_start_position[0].to_i, initial_start_position[1].to_i, initial_start_position[3])
 
-puts "Your rover's final position is #{rover.read_instructions(move_turn_instructions)}"
+
+  puts "Please enter your move and turn instructions. ex. LMRMMLM"
+  move_turn_instructions = gets.chomp.split("")
+  # puts "Your instructions are: #{move_turn_instructions}"
+
+rover = Rover.new(initial_start_position[0].to_i, initial_start_position[1].to_i, initial_start_position.to_s[2])
+
+ puts rover.read_instructions(move_turn_instructions)
