67cef99d5330e3a5124adbc1d077fa212fbb710a
Stella <stellaha4@gmail.com>
2016-06-06 13:45:33 -0400

    debugged program and it works

--- rover.rb
+++ rover.rb
@@ -2,14 +2,12 @@ class Rover
 
   attr_accessor :x_coordinate, :y_coordinate, :facing_direction
 
-  def initialize(x_coordinate, y_coordinate, facing_direction)
-    @x_coordinate = x_coordinate
-    @y_coordinate = y_coordinate
-    @facing_direction = facing_direction
-    puts @facing_direction
+  def initialize
+    @x_coordinate
+    @y_coordinate
+    @facing_direction
   end
 
-#4. def a method called read_instructions
   def read_instructions(instructions)
     instructions.each do |command|
       if command == "L" || command == "R"
@@ -22,20 +20,18 @@ class Rover
     return "you are at #{@x_coordinate} #{@y_coordinate} #{@facing_direction}"
   end
 
-#6. def a method called turn
   def turn(turn_direction)
-      if (@facing_direction == "N" && command == "L") || (@facing_direction == "S" && command == "R")
+      if (@facing_direction == "N" && turn_direction == "L") || (@facing_direction == "S" && turn_direction == "R")
         @facing_direction = "W"
-      elsif (@facing_direction == "E" && command == "L") || (@facing_direction == "W" && command == "R")
+      elsif (@facing_direction == "E" && turn_direction == "L") || (@facing_direction == "W" && turn_direction == "R")
         @facing_direction = "N"
-      elsif (@facing_direction == "S" && command == "L") || (@curret_direction == "N" && command == "R")
-        @facing_direction == "E"
-      elsif (@facing_direction == "W" && command == "L") || (@facing_direction == "E" && command == "R")
+      elsif (@facing_direction == "S" && turn_direction == "L") || (@facing_direction == "N" && turn_direction == "R")
+        @facing_direction = "E"
+      elsif (@facing_direction == "W" && turn_direction == "L") || (@facing_direction == "E" && turn_direction == "R")
         @facing_direction = "S"
       end
   end
 
-#7. def a method called move
   def move
     if @facing_direction == "N"
       @y_coordinate += 1
@@ -60,12 +56,13 @@ end
   initial_start_position = gets.chomp.split(" ")
   puts "Your starting position is #{initial_start_position}"
 
-
-
   puts "Please enter your move and turn instructions. ex. LMRMMLM"
   move_turn_instructions = gets.chomp.split("")
-  # puts "Your instructions are: #{move_turn_instructions}"
 
-rover = Rover.new(initial_start_position[0].to_i, initial_start_position[1].to_i, initial_start_position.to_s[2])
+  rover = Rover.new
+  rover.x_coordinate=initial_start_position[0].to_i
+  rover.y_coordinate=initial_start_position[1].to_i
+  rover.facing_direction=initial_start_position[2]
+
 
  puts rover.read_instructions(move_turn_instructions)
